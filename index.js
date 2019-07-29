d7889348f9c144bfbe321c6cfdbc9ebce7cd0196
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-29 15:19:38 -0700

    still working on index.js file

--- index.js
+++ index.js
@@ -23,3 +23,29 @@ server.post('/api/users', (req, res) => {
         });
 });
 
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, err });
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.findById(id)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user);
+            }else {
+                res.status(404).json({ success: false, error: 'The user with the specified ID does not exist.'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, error 'The user information could not be retrieved.', err })
+        });
+});
+
