1d49e5fcd01c49185252ae48c7296af5f97f14af
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-29 15:00:33 -0700

    working on index.js file

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+
+    db.insert(user)
+        .then(users => {
+            if(!user.name || !user.bio) {
+                res.status(400).json({ success: false, errorMessage: 'PLease provide name and bio for the user.'});
+            } else {
+                res.status(201).json({ success: true, users });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, error: 'Therewas an error while saving the user to the database. ', err});
+        });
+});
+
